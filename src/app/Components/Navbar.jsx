371ef4df912b2983e406f1cb79613f8f6b627a67
mahamudul hasan seber <sebermh@gmail.com>
2025-08-15 00:41:29 +0600

    Linking And Navigation

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div>
       <nav className="flex justify-center">
         <ul className="flex justify-between w-1/2">
-          <li>Home</li>
-          <li>Services</li>
-          <li>About</li>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="services">
+            <li>Services</li>
+          </Link>
+          <Link href="about">
+            <li>About</li>
+          </Link>
         </ul>
       </nav>
     </div>
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,28 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const AboutPage = () => {
+  const router = useRouter();
+  const isLoggedIn = false;
+  const handleNavigate = () => {
+    if (isLoggedIn) {
+      router.push("/about/address");
+    } else {
+      router.push("/");
+    }
+  };
   return (
     <div>
       <p className="font-bold text-3xl"> About Pages</p>
+      <p>
+        <Link href="/about/address">Address Page</Link>
+      </p>
+
+      <button type="button" onClick={handleNavigate}>
+        Address Page
+      </button>
     </div>
   );
 };
